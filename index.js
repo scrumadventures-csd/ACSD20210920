2790697f596fb8640aaad3fa6dbdeb3f8d4f7ef0
Griffin McManus <griffinbrycem@gmail.com>
2021-09-17 13:32:48 -0700

    Server set up

--- index.js
+++ index.js
@@ -2,18 +2,15 @@ const express = require('express')
 const cors = require('cors')
 const app = express();
 
-require('./config.mongoose.config')
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
     res.send('Running')
 })
 
 
-app.listen(8000, () => {
-    console.log("Listening on port: 8000");
-})
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
